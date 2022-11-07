d9d63a6bacec6d85112c34bf866fc1680607e071
Ralph Effting <ralfting@gmail.com>
2022-11-07 13:11:13 -0300

    Add firebase config

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,124 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItemSliding,
+  IonItem,
+  IonLabel,
+  IonItemOptions,
+  IonItemOption,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonModal,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonInput
+} from "@ionic/react";
+
+import { add } from "ionicons/icons";
+
+import "./Tab1.css";
+
+interface Task {
+  id: number;
+  name: string;
+}
 
 const Tab1: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [taskName = "", setTaskName] = useState<string>();
+
+  function addNewTask() {
+    const new_id = tasks.length + 1;
+
+    const newTask = {
+      id: new_id,
+      name: taskName
+    };
+
+    tasks.push(newTask);
+
+    setTasks(tasks);
+
+    setTaskName("");
+
+    setShowModal(false);
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle color="primary">Task Manager</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent>
+        <IonList>
+          {tasks.length > 0 ? (
+            tasks.map((item: Task) => {
+              return (
+                <IonItemSliding key={item.id}>
+                  <IonItem className="todo-item">
+                    <IonLabel>{item.name}</IonLabel>
+                  </IonItem>
+                  <IonItemOptions side="end">
+                    <IonItemOption onClick={() => {}}>Done</IonItemOption>
+                  </IonItemOptions>
+                </IonItemSliding>
+              );
+            })
+          ) : (
+            <IonItem>
+              <IonLabel color="danger">
+                You have not yet added tasks for today
+              </IonLabel>
+            </IonItem>
+          )}
+        </IonList>
+
+        {/* Modal*/}
+        <IonModal isOpen={showModal}>
+          <IonCard>
+            <IonItem>
+              <IonLabel color="primary">Add New Task</IonLabel>
+            </IonItem>
+
+            <IonCardContent>
+              <IonItem>
+                <IonInput
+                  value={taskName}
+                  placeholder="Enter Task Name..."
+                  onIonChange={(e) => setTaskName(e.detail.value!)}
+                ></IonInput>
+              </IonItem>
+
+              <IonButton
+                expand="full"
+                color="success"
+                onClick={() => addNewTask()}
+              >
+                Add Task
+              </IonButton>
+            </IonCardContent>
+          </IonCard>
+          <IonButton color="success" onClick={() => setShowModal(false)}>
+            Close Modal
+          </IonButton>
+        </IonModal>
+
+        {/* FAB */}
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton color="success" onClick={() => setShowModal(true)}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
